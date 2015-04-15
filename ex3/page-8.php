79eb797f31b7e366cffd1435022ae086ba6b30e5
benimue <muelbenj@student.ethz.ch>
2015-04-15 21:11:24 +0200

    Ex 3.2 - Send Mail Fix

--- ex3/page-8.php
+++ ex3/page-8.php
@@ -1,9 +1,16 @@
 <?php 
     global $stylesheet_dir, $stylesheet_url;
-
+    $mail_sent = FALSE;
     get_header();
+    	if (isset($_POST['submit']))
+	{
+		$first_name = $_POST['firstname'];
+		$last_name = $_POST['lastname'];
+		$mail_text = $_POST['message'];
+		
+		$mail_sent = wp_mail(get_option('admin_email'), "Message from ".$first_name." ".$last_name, $mail_text);
+	};
 ?>
-
 				<div class="container" style="background-image: url('<?php echoPicture($stylesheet_dir,'images/bg4.png');?>');background-size: 100%;background-repeat: no-repeat;background-color: #040205; min-height: 500px;" role="main">
 				
 					<div style="width:100%">
@@ -15,9 +22,9 @@
 					<br><br><br><br><br><br><br><br><br><br><br>
 
 					<div style="float:left;overflow: hidden;vertical-align: bottom;">
-						<form>
+						<form method="post" action="<?php echo the_permalink();?>">
 							<p style="float:left;">
-								First name:
+								First name test:
 							</p>
 
 							<input type="text" name="firstname" style="float:right;">
@@ -31,10 +38,15 @@
 							<br>
 							<textarea type="text" name="message" rows="7" cols="65" ></textarea>
 							<br>
-							<button type="button" style="background-color:#fafafa; color:#000;padding-left:5px;padding-right:5px;">
+							<button name="submit" style="background-color:#fafafa; color:#000;padding-left:5px;padding-right:5px;">
 								Submit
 							</button>
 						</form>
+						<p style="color: blue; background: white">
+								<?php if ( $mail_sent){
+									echo "Mail has been sent to ".get_option("blogname");
+								} ?>
+						</p>
 					</div>
 					<br>
 
