daa39d7e383bae4ae57c15988a89d95707152eca
Wellittem <matteo.pozzetti@gmail.com>
2015-04-19 19:47:00 +0200

    Ex4 - Part 1

--- ex4/base/js/functions.js
+++ ex4/base/js/functions.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
 	var map = { "img1" : 1, "img2" : 2, "img3" : 3 , "img4" : 4}
 	var map_info = { "img1" : "ETH Terrasse", "img2" : "ETH Main Building, interior", "img3" : "ETH Main Building", "img4" : "ETH Main Building 2"}
 
-	$("body").tiltandtap({
-		onTiltRight    : changeSelectionRight,
-		onTiltLeft 	   : changeSelectionLeft
-	}); 
+	$('#big_img').hide();
 	
-	$('#btn').click(function () {
-		changeSelectionRight();
-	}); 
+	// *********** IMAGE SELECTION *************
+	
+	$("#touch").tiltandtap({
+		onTiltRight    : changeSelectionRight,
+		onTiltLeft 	   : changeSelectionLeft,
+		tiltUp    : { onTiltUp: showImage, interaction : {type: "press", element: "touch"}},
+		tiltDown  : { onTiltDown: hideImage, interaction : {type: "press", element: "touch"}}
+	});
 	
 	function changeSelectionRight ()
 	{
@@ -37,4 +39,21 @@ $(document).ready(function() {
 		}
 		
 	}
+	
+	// *********** BIGGER IMAGE *************
+	
+	$('#btn').click(function () {
+		showImage();
+	}); 
+	
+	
+	function showImage () {
+		var img = $(".selected").attr("src");
+		$('#big_img').attr("src", img);
+		$('#big_img').show();
+	}
+	function hideImage () {
+		$('#big_img').hide();
+	}
+	
 });
