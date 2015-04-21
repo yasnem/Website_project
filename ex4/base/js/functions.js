92bcbfd073161204b6fa8296a77797611a189cad
benimue <muelbenj@student.ethz.ch>
2015-04-21 15:15:40 +0200

    Ex 4 - current text
    -

--- ex4/base/js/functions.js
+++ ex4/base/js/functions.js
@@ -2,13 +2,14 @@ $(document).ready(function() {
 	var map = { "img1" : 1, "img2" : 2, "img3" : 3 , "img4" : 4}
 	var map_info = { "img1" : "ETH Terrasse", "img2" : "ETH Main Building, interior", "img3" : "ETH Main Building", "img4" : "ETH Main Building 2"}
 	
-	var visible = false;
+	var visibleImg = false;
+	var visibleText = false;
 	$('#big_img').hide();
 	
 	// *********** IMAGE SELECTION *************
 	
 	$("#touch").tiltandtap({
-		onTiltRight    : changeSelectionRight,
+		onTiltRight    : changeSelectionRight, vibrationFeedback : 500, 
 		onTiltLeft 	   : changeSelectionLeft,
 		tiltUp    : { onTiltUp: showImage, interaction : {type: "press", element: "touch"}},
 		tiltDown  : { onTiltDown: hideImage, interaction : {type: "press", element: "touch"}}
@@ -25,7 +26,8 @@ $(document).ready(function() {
 			$('#'+child).addClass("selected");
 			$('#'+child).removeClass("notselected");
 		}
-		if(visible) showImage();
+		if(visibleImg) showImage();
+		updateText();
 	}
 	function changeSelectionLeft ()
 	{
@@ -38,25 +40,32 @@ $(document).ready(function() {
 			$('#'+child).addClass("selected");
 			$('#'+child).removeClass("notselected");
 		}
-		if(visible) showImage();
+		if(visibleImg) showImage();
+		updateText();
 	}
 	
 	// *********** BIGGER IMAGE *************
 	
 	$('#btn').click(function () {
 		showImage();
-	}); 
-	
+	});
 	
 	function showImage () {
 		var img = $(".selected").attr("src");
 		$('#big_img').attr("src", img);
 		$('#big_img').show();
-		visible = true;
+		visibleImg = true;
 	}
 	function hideImage () {
 		$('#big_img').hide();
-		visible = false;
+		visibleImg = false;
 	}
 	
+	// *********** BIGGER IMAGE *************
+	
+	function updateText() {
+		var img = $(".selected").attr("src");
+		img = img.replace("img/", "").replace(".jpg", "");
+		$("#info").text(map_info[img]);
+	}
 });
