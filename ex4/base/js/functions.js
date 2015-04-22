b06c53663e547dd00767aec6a99c887cbeb71214
benimue <muelbenj@student.ethz.ch>
2015-04-22 09:35:34 +0200

    Ex 4 - Extend the Plugin
    implementation of function, that recognizes the up-and-down motion (but
    NOT as a gesture)

--- ex4/base/js/functions.js
+++ ex4/base/js/functions.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
 	var map_info = { "img1" : "ETH Terrasse", "img2" : "ETH Main Building, interior", "img3" : "ETH Main Building", "img4" : "ETH Main Building 2"}
 	
 	var visibleImg = false;
-	var visibleText = false;
+	
 	$('#big_img').hide();
+	
+	var upFlag = false;
+	var startTime = 0;
+	var visibleText = false;
 	
 	// *********** IMAGE SELECTION *************
 	
 	$("#touch").tiltandtap({
-		onTiltRight    : changeSelectionRight, vibrationFeedback : 500, 
+		onTiltRight    : changeSelectionRight,
 		onTiltLeft 	   : changeSelectionLeft,
 		tiltUp    : { onTiltUp: showImage, interaction : {type: "press", element: "touch"}},
-		tiltDown  : { onTiltDown: hideImage, interaction : {type: "press", element: "touch"}}
+		tiltDown  : { onTiltDown: hideImage, interaction : {type: "press", element: "touch"}},
+		onTiltUp    : setUpFlag,
+		onTiltDown  : checkUpFlag,
 	});
 	
 	function changeSelectionRight ()
@@ -67,5 +73,22 @@ $(document).ready(function() {
 		var img = $(".selected").attr("src");
 		img = img.replace("img/", "").replace(".jpg", "");
 		$("#info").text(map_info[img]);
+	}
+	
+	function setUpFlag() {
+		var d = new Date();
+		startTime = d.getTime();
+		upFlag = true;
+	}
+	
+	function checkUpFlag() {
+		var d = new Date();
+		var endTime = d.getTime();
+		
+		if ((endTime - startTime) <= 500) {
+			updateText();
+			$("#info").toggle();
+		}
+		upFlag = false;
 	}
 });
